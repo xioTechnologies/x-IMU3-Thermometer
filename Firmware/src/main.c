6c95ee90397cd264be27ec0f0a5cf35c5e4255b3
x-io Technologies <social@x-io.co.uk>
2024-08-11 15:38:07 +0100

    Update x-io PIC32 library

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -37,7 +37,7 @@ int main(void) {
 
     // Print start up message
     ResetCausePrint(ResetCauseGet());
-    printf("x-IMU3-Thermometer v1.0.1\n");
+    printf("x-IMU3-Thermometer v1.0.2\n");
 
     // Initialise modules
     TimerInitialise();
